159d975f7bb78e035323c2811e5092f967677a0b
iso-1 <kd1242983@st.kobedenshi.ac.jp>
2017-11-15 15:54:12 +0900

    前期末発表から未記入の現状

--- www/js/app.js
+++ www/js/app.js
@@ -21,31 +21,6 @@ function addTodo(camera_url) {
     $('#timer').yycountdown({endDateTime:'2020/07/24 00:00:00'});
 };
 
-function() {'use strict'
-    
-    var timer = document.getElementById('timer')
-    var min = document.getElementById('min')
-    var sec = document.getElementById('sec')
-    var reset = document.getElementById('reset')
-    var start = document.getElementById('start')
-
-    var startTime;
-    var vtimeLeft;
-    var timeToCountDown = 4 *1000;
-    
-    function updateTimer(t) {
-        var d = new Date(t);
-        var m = d.getMinutes();
-        var s = d.getSeconds();
-        var ms = d.getMilliseconds();
-        timer.textContent = m + ':' + s + '.' + ms;
-    }
-    
-    function countdown() {
-        setTimeout(function() {
-        timeLeft = timeToCountDown - (Date.now() - starTime);
-        }, 10);
-
 
 var strDate = $("date-deadline").text();  // HTMLフォームの文字列
 var date = new Date(strDate);  // 日付
